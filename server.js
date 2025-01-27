58a66108216eda3688f9a7acb9bec381557eae62
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2025-01-27 17:09:03 +0530

    feat:create project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require('fs');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+
+const app = express();
+app.use(cors());
+const port = 3070;
+
+//Connection
+mongoose
+  .connect("mongodb://localhost:27017/resume_builder")
+  .then(() => console.log("mongodb connected to resume_builder"))
+  .catch((err) => console.log("mongo error at resume_builder"));
+
+// const User = mongoose.model("resume_builder", userSchema);
+
