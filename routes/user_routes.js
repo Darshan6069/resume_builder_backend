73c8d05a9e8fddb3cac077f884f637c3dcfed039
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2025-03-19 15:10:34 +0530

    feat:changes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = express.Router();
 const userController = require("../controllers/user_controller");
 const { verifyToken } = require("../middleware/middlewares");
-
-router.post("/personal-info", verifyToken, userController.addPersonalInfo);
+const router = express.Router();
+router.post("/personal_info", verifyToken, userController.addPersonalInfo);
 router.post("/education_info", verifyToken, userController.addEducationInfo);
 router.post("/experience_info", verifyToken, userController.addExperienceInfo);
+router.post("/project_info", verifyToken, userController.addProjectInfo);
 
 module.exports = router;
